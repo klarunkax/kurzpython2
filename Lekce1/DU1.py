0672ce8fc643e6fa756455a76353e5d732745c8e
Klara Janouskova <klarunkax@gmail.com>
2021-10-18 11:46:52 +0200

    lekce3

--- Lekce1/DU1.py
+++ Lekce1/DU1.py
@@ -9,13 +9,13 @@ r = requests.get("https://raw.githubusercontent.com/pesikj/progr2-python/master/
 open("titanic.csv", 'wb').write(r.content)
 
 # 1.TITANIC
-# titanic = pandas.read_csv("titanic.csv")
-#
-# print(titanic.head())
-# titanic.info()
-#
-# titanic_pivot = pandas.pivot_table(titanic, index="Pclass",columns="Sex", values="Survived",aggfunc=numpy.sum)
-# print(titanic_pivot)
+titanic = pandas.read_csv("titanic.csv")
+
+print(titanic.head())
+titanic.info()
+
+titanic_pivot = pandas.pivot_table(titanic, index="Pclass",columns="Sex", values="Survived",aggfunc=numpy.sum)
+print(titanic_pivot)
 
 #2.PUJČOVÁNÍ KOL
 london_merged = pandas.read_csv("london_merged.csv")
@@ -26,5 +26,5 @@ london_merged["year"] = london_merged["timestamp"].dt.year
 print(london_merged.head())
 london_merged.info()
 
-london_merged_pivot = pandas.pivot_table(london_merged, index="weather_code",columns="year", values="weather_code")
+london_merged_pivot = pandas.pivot_table(london_merged, index="weather_code",columns="year", values="cnt",aggfunc=numpy.sum)
 print(london_merged_pivot)
